638429a992bb114ab448453a0f269fe2e9364a75
Sudeep <sudeep.rp@gmail.com>
2025-07-29 12:39:13 +0530

    average of non-empty input

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,17 @@
 #include "stats.h"
+#include <numeric>
+#include <limits>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+struct Stats {
+    float average;
+    float max;
+    float min;
+};
+
+Stats Statistics::ComputeStatistics(const std::vector<float>& readings) {
+    Stats stats;
+    stats.average = std::accumulate(readings.begin(), readings.end(), 0.0f) / readings.size();
+    stats.max = *std::max_element(readings.begin(), readings.end());
+    stats.min = *std::min_element(readings.begin(), readings.end());
+    return stats;
 }
